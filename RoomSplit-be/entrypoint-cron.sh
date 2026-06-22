4901569b479ce0ea44f91c3a7030d1721a7057f9
Lorenzo Pelone <pelone.lorenzo07@gmail.com>
2026-06-22 22:54:35 +0200

    Sistemata documentazione, automatizzata creazione spese ricorrenti, aggiornate statistiche personali

---
+++ RoomSplit-be/entrypoint-cron.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# entrypoint-cron.sh
+
+# Esporta le variabili d'ambiente per cron
+printenv | grep -v "no_proxy" >> /etc/environment
+
+# Crea il crontab che esegue il comando ogni giorno a mezzanotte (00:00)
+# e redireziona l'output verso il log standard di Docker
+echo "0 0 * * * /usr/local/bin/python /app/manage.py genera_spese > /proc/1/fd/1 2>/proc/1/fd/2" > /etc/cron.d/django-cron
+
+# Dai i permessi
+chmod 0644 /etc/cron.d/django-cron
+crontab /etc/cron.d/django-cron
+
+# Avvia il demone cron in foreground perché il container resti in vita
+cron -f
